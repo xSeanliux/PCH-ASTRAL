07568a3014434ee2cb3fd83e2112ecddd978842e
Sean Liu <zhxnliu@yahoo.com.tw>
2026-05-18 00:09:01 -0500

    more teest

--- tests/scripts/lib/test_pch.py
+++ tests/scripts/lib/test_pch.py
@@ -3,7 +3,7 @@ from scripts.lib.pch import Quartet, Dataset, Character, PCH_ASTRAL_W
 import polars as pl
 
 
-def test_normalise_both_sides():
+def test_quartet_normalise_both_sides():
     assert Quartet.normalise((1, 2, 3, 4)) == (1, 2, 3, 4)
     assert Quartet.normalise((2, 1, 3, 4)) == (1, 2, 3, 4)
     assert Quartet.normalise((1, 2, 4, 3)) == (1, 2, 3, 4)
@@ -11,14 +11,19 @@ def test_normalise_both_sides():
     assert Quartet.normalise((2, 1, 4, 3)) == (1, 2, 3, 4)
 
 
-def test_extract_throws_on_wrong_first_cols():
+def test_quartet_taxon_set():
+    q = ("a", "b", "c", "d")
+    return Quartet(q).taxon_set == set(q)
+
+
+def test_dataset_extract_throws_on_wrong_first_cols():
     df = pl.DataFrame({"id": [1, 2], "feature": [3, 3]})
     with pytest.raises(AssertionError) as e:
         Dataset._extract_names_and_chrs(df)
     assert "expect first three cols" in str(e)
 
 
-def test_extract_names_and_chars_output():
+def test_dataset_extract_names_and_chars_output():
     df = pl.DataFrame(
         {
             "id": ["a", "b", "c"],
