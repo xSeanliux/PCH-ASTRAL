392004d1d350af54df9b5d895ebf9dd800d03631
Sean Liu <zhxnliu@yahoo.com.tw>
2026-06-07 23:55:49 -0500

    udpate install

--- scripts/sh/installs/install_lingphylosimulator.sh
+++ scripts/sh/installs/install_lingphylosimulator.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 git clone https://github.com/marccanby/LingPhyloSimulator.git bin/LingPhyloSimulator --quiet
 
-CLASSPATH=""
-JAROUT=bin/out
+CLASSPATH="bin/LingPhyloSimulator"
+JAROUT="bin/LingPhyloSimulator/out"
 
-for FILE in bin/LingPhyloSimulator/lib/*.jar; do
+for FILE in bin/lingphylosimulator_jars/*.jar; do
     CLASSPATH=$FILE:$CLASSPATH
+    jar xf "$FILE"
 done
 echo $CLASSPATH
 
 javac -cp $CLASSPATH -d $JAROUT bin/LingPhyloSimulator/*.java 
 
+
+jar cfe bin/LingPhyloSimulator.jar Simulator $JAROUT/Simulator.class bin/LingPhyloSimulator/Main bin/lingphylosimulator_jars
 # rm -rf bin/LingPhyloSimulator
