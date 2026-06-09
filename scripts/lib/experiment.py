3e68dcb6bcfa654f18e9fff019db93c349cc9898
Sean Liu <zhxnliu@yahoo.com.tw>
2026-06-08 22:10:11 -0500

    add experiment

---
+++ scripts/lib/experiment.py
@@ -0,0 +1,58 @@
+import yaml
+from pydantic import BaseModel, Field
+from scripts.lib.types import Polymorphism
+from enum import StrEnum
+
+
+class SimulationConfigSetting(BaseModel):
+    poly: Polymorphism
+    homoplasy_factor: float
+    tree_height: int
+    n_chars: int
+
+
+class ExperimentSimulationConfig(BaseModel):
+    n_horizontal_edges: list[int]
+    n_trees: int
+    n_replicas: int
+    n_taxa: int
+    simulation_configs: list[SimulationConfigSetting]
+
+
+class ASTRAL3Config(BaseModel):
+    class BipartitionStrategy(StrEnum):
+        BINARY_CHARACTER = "binary_character"
+        MP4_TREES = "mp4_trees"
+        GA_TREES = "ga_trees"
+
+    bipartition_strategies: list[BipartitionStrategy] = Field(list())
+    is_exact: bool
+
+
+class WeightedASTRALConfig(BaseModel): ...
+
+
+class WeightedTreeQMCConfig(BaseModel):
+    class NormalisationStrategy(StrEnum):
+        N2 = "n2"
+
+    normalisation_strategy: NormalisationStrategy
+
+
+class MP4Config(BaseModel): ...
+
+
+class GAConfig(BaseModel): ...
+
+
+class MethodConfig(BaseModel):
+    astral_3: ASTRAL3Config | None = Field(None)
+    wastral: WeightedASTRALConfig | None = Field(None)
+    w_tree_qmc: WeightedTreeQMCConfig | None = Field(None)
+    mp4: MP4Config | None = Field(None)
+    gray_atkinson: GAConfig | None = Field(None)
+
+
+class ExperimentConfig(BaseModel):
+    simulation: ExperimentSimulationConfig
+    methods: MethodConfig
